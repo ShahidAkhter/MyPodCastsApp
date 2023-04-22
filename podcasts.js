c1d4ed16ef83badf33b4347da966c12c4ec30987
Shahid Akhter <112084911+ShahidAkhter@users.noreply.github.com>
2023-04-22 15:25:51 +0530

    Adding Content in podcasts
    Adding Content in podcasts

--- podcasts.js
+++ podcasts.js
@@ -1,18 +1,26 @@
 let podcasts = [
     {
         title: "On My Way",
-        path: "https://drive.google.com/uc?export=view&id=1g_AOg4PiA-5aGfdgurxgBOjMal7pCkMS",
+        path: "https://drive.google.com/uc?export=view&id=1NJ6stImVd6ErORsyGqnyAuvyE46osqal",
         cover: "https://c.saavncdn.com/866/On-My-Way-English-2019-20190308195918-500x500.jpg",
         creator: "Alan Walker, Sabrina Carpenter & Farruko",
         channel: "Alan Walker",
-        length:""
+        length:"",
     },
     {
         title: "Good day for dreaming",
-        path: "https://drive.google.com/uc?export=view&id=1gozd6SKWhOZicnYUcYRHdspTgy7PQxNt",
+        path: "https://drive.google.com/uc?export=view&id=1NGv0tuGdb_OxvtPDWjuXWhjgSMbGRFRN",
         cover: "https://i.scdn.co/image/ab67616d0000b273a6de26a824ef5f64569be17b",
         creator: "Ruelle",
         channel: "Ruelle",
-        length:""
-    }
+        length:"",
+    },
+    {
+        title: "Grew up at midnight",
+        path: "https://drive.google.com/uc?export=view&id=1NHy4w36YXyaAr4y2eutgH3tZKtyOVCdL",
+        cover: "https://images.genius.com/8aa917b2006771e389754ab622eaf918.1000x1000x1.jpg",
+        creator: "The Maccabees",
+        channel: "The Maccabees",
+        length:"",
+    },
 ];
